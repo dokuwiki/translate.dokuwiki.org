6966fbf32084cf9ea8be0218565dfbd127d16a63
Dominik Eckelmann <deckelmann@gmail.com>
2013-02-12 13:46:20 +0100

    improved startpage

--- src/org/dokuwiki/translatorBundle/Controller/PluginController.php
+++ src/org/dokuwiki/translatorBundle/Controller/PluginController.php
@@ -5,9 +5,6 @@ namespace org\dokuwiki\translatorBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PluginController extends Controller {
-    public function indexAction() {
-        return $this->render('dokuwikiTranslatorBundle:Plugin:index.html.twig');
-    }
 
     public function addAction() {
         return $this->render('dokuwikiTranslatorBundle:Plugin:add.html.twig');
