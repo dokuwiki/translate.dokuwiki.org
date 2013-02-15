13bd0210cbef2f9b88b27537fdee5162aa878adc
Dominik Eckelmann <deckelmann@gmail.com>
2013-02-15 11:32:57 +0100

    added plugin detail page

--- src/org/dokuwiki/translatorBundle/Controller/DefaultController.php
+++ src/org/dokuwiki/translatorBundle/Controller/DefaultController.php
@@ -8,4 +8,9 @@ class DefaultController extends Controller {
     public function indexAction() {
         return $this->render('dokuwikiTranslatorBundle:Default:index.html.twig');
     }
+
+    public function showAction($name) {
+        return $this->render('dokuwikiTranslatorBundle:Default:show.html.twig',
+            array('name' => $name));
+    }
 }
