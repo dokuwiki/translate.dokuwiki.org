4f8e0dc1661757ba7995d6ca897242bed60039f7
Dominik Eckelmann <deckelmann@gmail.com>
2013-04-12 16:05:38 +0200

    show real data on overview

--- src/org/dokuwiki/translatorBundle/Controller/DefaultController.php
+++ src/org/dokuwiki/translatorBundle/Controller/DefaultController.php
@@ -3,10 +3,28 @@
 namespace org\dokuwiki\translatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use org\dokuwiki\translatorBundle\Entity\RepositoryEntity;
 
 class DefaultController extends Controller {
     public function indexAction() {
-        return $this->render('dokuwikiTranslatorBundle:Default:index.html.twig');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $data['coreRepository'] = $entityManager->createQuery(
+            'SELECT repository
+             FROM dokuwikiTranslatorBundle:RepositoryEntity repository
+             WHERE repository.type = \'core\'')->getSingleResult();
+
+        $query = $entityManager->createQuery('SELECT repository
+             FROM dokuwikiTranslatorBundle:RepositoryEntity repository
+             WHERE repository.type != \'core\'
+             AND repository.state = :state
+             ORDER BY repository.popularity DESC
+             '
+        );
+        $query->setParameter('state', RepositoryEntity::$STATE_ACTIVE);
+
+        $data['repositories'] = $query->getResult();
+        return $this->render('dokuwikiTranslatorBundle:Default:index.html.twig', $data);
     }
 
     public function showAction($name) {
