0357209d017eb4e40e93af507d2f0328486cdb6a
Dominik Eckelmann <deckelmann@gmail.com>
2013-05-10 14:48:40 +0200

    removed unused files

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Content-Type: text/plain; Charset=UTF-8');
-//$data = unserialize(file_get_contents('D:\Temp\test 1\core\dokuwiki\lang\af.ser'));
-//print_r($data);
-
-unlink('D:\Temp\test 1\core\dokuwiki\tmp\17\.git\objects\ff\2f9a5d61c959294df8f6df55619bb97a9b31c4');
