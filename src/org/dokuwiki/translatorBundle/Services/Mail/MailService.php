dcb60950b940e15f4c4f213ec3e691272632a381
Dominik Eckelmann <deckelmann@gmail.com>
2013-07-31 12:19:58 +0200

    added email logging on dev environment

--- src/org/dokuwiki/translatorBundle/Services/Mail/MailService.php
+++ src/org/dokuwiki/translatorBundle/Services/Mail/MailService.php
@@ -1,6 +1,8 @@
 <?php
 namespace org\dokuwiki\translatorBundle\Services\Mail;
 
+use Monolog\Logger;
+
 class MailService {
 
     /**
@@ -18,15 +20,22 @@ class MailService {
      */
     private $from;
 
-    function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig, $from) {
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig, $from, Logger $logger) {
         $this->mailer = $mailer;
         $this->template = $twig;
         $this->from = $from;
+        $this->logger = $logger;
     }
 
     public function sendEmail($to, $subject, $template, $data = array()) {
         $message = $this->createMessage($to, $subject, $template, $data);
 
+        $this->logMail($message);
         $this->mailer->send($message);
     }
 
@@ -46,6 +55,17 @@ class MailService {
         $message->setFrom($this->from);
         $message->setBody($this->template->render($template, $data));
         return $message;
+    }
+
+    private function logMail(\Swift_Message $message) {
+
+        $context = array();
+        $context['to'] = $message->getTo();
+        $context['subject'] = $message->getSubject();
+        $context['text'] = $message->getBody();
+
+        $this->logger->debug(sprintf('Sending mail'), $context);
+
     }
 
 }
