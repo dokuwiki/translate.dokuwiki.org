107960165bbab25666a02e764d68d144d4309396
Dominik Eckelmann <deckelmann@gmail.com>
2013-08-01 16:52:32 +0200

    corrected exception

--- src/org/dokuwiki/translatorBundle/Services/Repository/RepositoryNotUpdatedException.php
+++ src/org/dokuwiki/translatorBundle/Services/Repository/RepositoryNotUpdatedException.php
@@ -1,8 +1,6 @@
 <?php
 namespace org\dokuwiki\translatorBundle\Services\Repository;
 
-use SebastianBergmann\Exporter\Exception;
-
-class RepositoryNotUpdatedException extends Exception {
+class RepositoryNotUpdatedException extends \Exception {
 
 }
