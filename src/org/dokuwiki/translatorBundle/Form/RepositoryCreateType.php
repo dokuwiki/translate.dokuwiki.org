770a0e4d5d66200356cd4e1b1df384701a3df876
Dominik Eckelmann <deckelmann@gmail.com>
2013-08-01 14:31:16 +0200

    changed labels on form add plugin

--- src/org/dokuwiki/translatorBundle/Form/RepositoryCreateType.php
+++ src/org/dokuwiki/translatorBundle/Form/RepositoryCreateType.php
@@ -9,9 +9,9 @@ class RepositoryCreateType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder->add('name', 'text', array('label' => 'Plugin name'));
-        $builder->add('email');
-        $builder->add('url');
-        $builder->add('branch');
+        $builder->add('email', 'text', array('label' => 'E-mail'));
+        $builder->add('url', 'text', array('label' => 'Git url'));
+        $builder->add('branch', 'text', array('label' => 'Main branch'));
     }
 
     /**
