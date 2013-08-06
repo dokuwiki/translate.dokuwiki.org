6fcf2af947c7412c70130defc73c2d8389ab126b
Dominik Eckelmann <deckelmann@gmail.com>
2013-08-06 19:21:28 +0200

    added example of git url and renamed field, closes #33

--- src/org/dokuwiki/translatorBundle/Form/RepositoryCreateType.php
+++ src/org/dokuwiki/translatorBundle/Form/RepositoryCreateType.php
@@ -10,7 +10,7 @@ class RepositoryCreateType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder->add('name', 'text', array('label' => 'Plugin name'));
         $builder->add('email', 'text', array('label' => 'E-mail'));
-        $builder->add('url', 'text', array('label' => 'Git url'));
+        $builder->add('url', 'text', array('label' => 'Git clone url'));
         $builder->add('branch', 'text', array('label' => 'Main branch'));
     }
 
