ed5cdbd704942ac30bd77bce90d9296c801761da
Dominik Eckelmann <deckelmann@gmail.com>
2013-10-31 13:02:28 +0100

    highlight surrounding whitespaces on translation view, refs #54

---
+++ src/org/dokuwiki/translatorBundle/Twig/HighlightWhitespaceExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace org\dokuwiki\translatorBundle\Twig;
+
+class HighlightWhitespaceExtension extends \Twig_Extension {
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName() {
+        return 'highlight_whitespace';
+    }
+
+    public function getFilters() {
+        return array(
+            new \Twig_SimpleFilter('highlight_whitespace', array($this, 'highlightWhitespace'),
+                array('pre_escape' => 'html', 'is_safe' => array('html')))
+        );
+    }
+
+    public function highlightWhitespace($text) {
+
+        $tagStart = '<span class="highlight-whitespace" title="Here are whitespaces - don\'t forget them in your translation">';
+
+        $text = preg_replace('/([ \t]+)\n/', $tagStart . '$1</span>' . "\n", $text);
+        $text = preg_replace('/\n([ \t]+)/', "\n$tagStart" . '$1</span>', $text);
+
+        return $text;
+    }
+}
