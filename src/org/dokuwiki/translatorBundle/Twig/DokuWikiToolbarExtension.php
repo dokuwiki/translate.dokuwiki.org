bdb2b2ea310867678a53ed12f2438f8ea5883a89
Dominik Eckelmann <deckelmann@gmail.com>
2013-11-02 18:55:48 +0100

    fixed dokuwiki toolbar
    switched back to old method

---
+++ src/org/dokuwiki/translatorBundle/Twig/DokuWikiToolbarExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace org\dokuwiki\translatorBundle\Twig;
+
+use Twig_SimpleFunction;
+
+class DokuWikiToolbarExtension extends \Twig_Extension {
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName() {
+        return 'dokuWikiToolbar';
+    }
+
+    public function getFunctions() {
+        return array(
+            new Twig_SimpleFunction('dokuWikiToolbar', array(&$this, 'dokuWikiToolbar')),
+        );
+    }
+
+    public function dokuWikiToolbar() {
+        $template = '/var/www/wiki/htdocs/lib/tpl/dokuwiki/dwtb.html';
+        if (file_exists($template)) {
+            include $template;
+        }
+    }
+}
