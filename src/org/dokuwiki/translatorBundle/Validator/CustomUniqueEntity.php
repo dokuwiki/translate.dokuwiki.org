732c3ce4ed122a301d2fd13edea9aead1191cfc2
Gerrit Uitslag <klapinklapin@gmail.com>
2016-06-08 23:01:29 +0200

    Extended Unique Entity validator with customized message

---
+++ src/org/dokuwiki/translatorBundle/Validator/CustomUniqueEntity.php
@@ -0,0 +1,15 @@
+<?php
+namespace org\dokuwiki\translatorBundle\Validator;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @Annotation
+ */
+class CustomUniqueEntity extends UniqueEntity {
+
+    public function validatedBy() {
+        return 'customuniqueentity';
+    }
+
+}
