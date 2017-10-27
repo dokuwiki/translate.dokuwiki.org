bd3af104244cb0806aa64c071d3c39fecd308a4a
Gerrit Uitslag <klapinklapin@gmail.com>
2017-10-27 21:11:07 +0200

    Rename class UpdateLanguagesCommand

--- src/org/dokuwiki/translatorBundle/Command/UpdateLanguagesCommand.php
+++ src/org/dokuwiki/translatorBundle/Command/UpdateLanguagesCommand.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PatchFormatCommand extends ContainerAwareCommand {
+class UpdateLanguagesCommand extends ContainerAwareCommand {
 
     protected function configure() {
         $this->setName('dokuwiki:updateLanguages')
