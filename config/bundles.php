1456284c1b373fe632bff4f32e23c1d9cb6094ff
Gerrit Uitslag <klapinklapin@gmail.com>
2023-03-24 00:52:33 +0100

    migrate twig

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
