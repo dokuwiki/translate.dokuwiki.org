1a3847b7230e0edb54612bd8ab5fada1518858cd
Gerrit Uitslag <klapinklapin@gmail.com>
2023-03-25 00:08:49 +0100

    migrate assets, further details for twig
    - not completely sure if this routing for error in twig is needed
    - order of TwigBundle matters, maybe no issue anymore for newer
    versions?
    - strict mode for autowiring is default

--- config/bundles.php
+++ config/bundles.php
@@ -3,10 +3,10 @@
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
