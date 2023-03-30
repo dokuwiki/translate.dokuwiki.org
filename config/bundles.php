744bb72c203d73e4de688f79ebd2c21296323b3c
Gerrit Uitslag <klapinklapin@gmail.com>
2023-03-30 22:41:17 +0200

    Install Doctrine Migrations to be used instead of doctrine:schema:update

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
