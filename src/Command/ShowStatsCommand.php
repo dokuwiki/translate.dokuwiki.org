13a8229defdfd071fd29e31971ee7871185ce350
Gerrit Uitslag <klapinklapin@gmail.com>
2023-04-02 18:42:16 +0200

    showStats command: entire error messages can be shown by requesting a specific repo
    Fixes #108

--- src/Command/ShowStatsCommand.php
+++ src/Command/ShowStatsCommand.php
@@ -3,11 +3,13 @@
 namespace App\Command;
 
 use DateTime;
+use Doctrine\ORM\NoResultException;
 use Exception;
 use App\Entity\RepositoryEntity;
 use App\Repository\RepositoryEntityRepository;
 use App\Services\Repository\RepositoryManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -34,27 +36,57 @@ class ShowStatsCommand extends Command {
 
     protected function configure(): void {
         $this
-            ->setDescription('Show some statistics for maintenance');
+            ->setDescription('Show some statistics for maintenance')
+            ->addArgument('type', InputArgument::OPTIONAL, 'template, plugin or core')
+            ->addArgument('name', InputArgument::OPTIONAL, 'repository name');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $name = $input->getArgument('name');
+        $type = $input->getArgument('type');
 
-        /** @var RepositoryEntity[] $repositories */
-        $repositories = $this->repositoryEntityRepository->findAll();
+        if(isset($name) && isset($type)) {
+            $repositoryTypes = [
+                RepositoryEntity::$TYPE_CORE,
+                RepositoryEntity::$TYPE_PLUGIN,
+                RepositoryEntity::$TYPE_TEMPLATE
+            ];
+            if (!in_array($type, $repositoryTypes)) {
+                $output->writeln(sprintf(
+                    'Type must be %s, %s or %s',
+                    RepositoryEntity::$TYPE_CORE,
+                    RepositoryEntity::$TYPE_PLUGIN,
+                    RepositoryEntity::$TYPE_TEMPLATE
+                ));
+                return 1;
+            }
+            try {
+                $repo = $this->repositoryEntityRepository->getRepository($type, $name);
+            } catch (NoResultException $e) {
+                $output->writeln('nothing found');
+                return 1;
+            }
+            $repositories[] = $repo;
+        } else {
+            /** @var RepositoryEntity[] $repositories */
+            $repositories = $this->repositoryEntityRepository->findAll();
+        }
 
         // header
         $output->writeln('found ' . count($repositories) . ' repositories');
-        $dashLine = str_repeat('-', 15-1) . ' '
-            . str_repeat('-', 45-1) . ' '
+        $dashLine = str_repeat('-', 9-1) . ' '
+            . str_repeat('-', 15-1) . ' '
+            . str_repeat('-', 35-1) . ' '
             . str_repeat('-', 18-1) . ' '
             . str_repeat('-', 20-1) . ' '
             . str_repeat('-', 4-1) . ' '
             . str_repeat('-', 15-1);
         $output->writeln($dashLine);
         $output->writeln(
-            sprintf('%-15s', 'name ')
-            . sprintf('%-45s', 'Display name ')
+            sprintf('%-9s', 'Type ')
+            . sprintf('%-15s', 'Name ')
+            . sprintf('%-35s', 'Display name ')
             . sprintf('%-18s', 'State ')
             . sprintf('%-20s', 'Last update')
             . sprintf('%-4s', 'Cnt')
@@ -62,16 +94,25 @@ class ShowStatsCommand extends Command {
         );
         $output->writeln($dashLine);
 
+        $count = count($repositories);
         foreach ($repositories as $repoEntity) {
             try {
+                $output->write(sprintf('%-9s', $repoEntity->getType() . ' '));
                 $output->write(sprintf('%-15s', $repoEntity->getName() . ' '));
-                $output->write(sprintf('%-45s', $repoEntity->getDisplayName() . ' '));
+                $output->write(sprintf('%-35s', substr($repoEntity->getDisplayName() . ' ', 0, 34)));
                 $output->write(sprintf('%-18s', $repoEntity->getState()));
 
                 $date = new DateTime("@{$repoEntity->getLastUpdate()}");
                 $output->write($date->format('Y-m-d H:i:s') . ' ');
                 $output->write(sprintf('%-4s', $repoEntity->getErrorCount() . ' '));
-                $output->write(str_replace("\n", "\n   ", $repoEntity->getErrorMsg()));
+                $errorMsg = str_replace("\n", "\n   ", $repoEntity->getErrorMsg());
+                if($count > 1) {
+                    $errorMsg = strlen($errorMsg) > 50 ? substr($errorMsg,0,50) . ' (more..) ': $errorMsg;
+                } else {
+                    $errorMsg = "see below\n   " . $errorMsg;
+                }
+                $output->write($errorMsg);
+
 
                 $repo = $this->repositoryManager->getRepository($repoEntity);
                 if (!$repo->hasGit()) {
