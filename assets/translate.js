e924561db8d3669c485329ed690fe0331904528f
Gerrit Uitslag <klapinklapin@gmail.com>
2023-04-22 16:28:46 +0200

    update js code to latest jquery and bootstrap 3
    * before detach was used, combined with every time readding of the rows
    in the table. However, rows are not detached properly anymore, resulting
    in duplicated rows.
    * replaced by only show/hiding rows
    * update regexp escaping
    * update to recent jquery, try to remove deprecated jquery functions
    * use bootstrap 3 for pagination
    * replace regexp for tracing sprintf() placeholders
    * add tip about usage of %1$s,%2$s,etc
    * highlight rows with warning popovers more then empty rows

--- assets/translate.js
+++ assets/translate.js
@@ -8,91 +8,94 @@ import './styles/app.less';
 
 // loads the jquery package from node_modules
 import $ from 'jquery';
-import 'autosize'; //originally at 1.16.7, via yarn only 1.18.13 available
 
 // start the Stimulus application
 import './bootstrap';
 
+import autosize from "autosize/dist/autosize";
+import 'bootstrap';
+
 // provides the translation warnings
 
-$(document).ready(function() {
+$(function() {
 
-    var TranslationTable = function() {
+    const TranslationTable = function () {
         this.table = $('table tbody');
-        this.allElements = $('table tbody tr');
+        this.allRows = $('table tbody tr');
         this.paginationNote = $('#pagination__note');
-        this.allElements.detach();
         this.filterText = '';
-        this.elements = $();
+        this.filteredRows = new Array(this.allRows.length).fill(true);
         this.drawFilterControls();
         this.startOver();
     };
 
 
     TranslationTable.prototype.startOver = function() {
-
-        this.elements.detach();
-        this.filter();
+        this.applyFilter();
 
         // all page related indexes are 1 based, only selectPage and draw are using 0 based index
         this.page = 1;
-        this.itemCount = this.elements.size();
         this.itemsPerPage = 30;
         this.lastPage = Math.ceil(this.itemCount / this.itemsPerPage);
         this.paginationSpace = 3;
-        this.pagination = $('.pagination');
+        this.pagination = $('.pag-nav');
         this.pagination.empty();
 
         // hide pagination on fewer items
-        this.setupEvents();
+        this.setupEvents(); //TODO probably only once needed, move to constructor?
         if (this.usePagination()) {
             this.drawPaginationBar();
         }
         this.drawPaginationBarHint();
-        this.draw();
+        this.drawTable();
     };
 
     TranslationTable.prototype.usePagination = function() {
         return this.itemCount > this.itemsPerPage * 2;
     };
 
-    TranslationTable.prototype.filter = function() {
-        if (this.filterText == '') {
-            this.elements = this.allElements;
-            this.elements.appendTo(this.table);
+    TranslationTable.prototype.applyFilter = function() {
+        this.itemCount = this.filteredRows.length;
+        if (this.filterText === '') {
+            this.filteredRows.fill(true);
             return;
         }
 
-        var newElements = [];
-        var regex = new RegExp(this.escapeRegExp(this.filterText), 'i');
-        this.allElements.each(function(index, val) {
-            if ($(this).attr('data-translation-key').match(regex)) {
-                newElements.push(this);
-                return;
+        const regex = new RegExp(this.escapeRegExp(this.filterText), 'i');
+        this.filteredRows.fill(false);
+        for (let i = 0; i < this.allRows.length; i++) {
+            let row = $(this.allRows.get(i));
+            if (row.attr('data-translation-key').match(regex)) {
+                this.filteredRows[i] = true;
+                continue;
             }
-            if ($(this).find('td:first').text().match(regex)) {
-                newElements.push(this);
-                return;
+            if (row.find('td:first').text().match(regex)) {
+                this.filteredRows[i] = true;
+                continue;
             }
-            if ($(this).find('td textarea').val().match(regex)) {
-                newElements.push(this);
+            if (row.find('td textarea').val().match(regex)) {
+                this.filteredRows[i] = true;
+                continue;
             }
-        });
-        this.elements = $(newElements);
-        this.elements.appendTo(this.table);
+            this.itemCount--;
+        }
     };
 
     /**
      * @see http://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+     * which points now to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
      */
-    TranslationTable.prototype.escapeRegExp = function(str) {
-        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    TranslationTable.prototype.escapeRegExp = function(string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
     };
 
-    TranslationTable.prototype.draw = function() {
-        this.elements.hide();
-        var startIndex = (this.page-1) * this.itemsPerPage;
-        var endIndex;
+    /**
+     * Shows the page set by this.page, considers the filtering
+     */
+    TranslationTable.prototype.drawTable = function() {
+        this.allRows.hide();
+        let startIndex = (this.page-1) * this.itemsPerPage;
+        let endIndex;
         if (this.usePagination()) {
             endIndex = startIndex + this.itemsPerPage;
             if (endIndex >= this.itemCount) {
@@ -102,38 +105,47 @@ $(document).ready(function() {
             endIndex = this.itemCount;
         }
 
-        for (var i = startIndex; i < endIndex; i++) {
-            var element = jQuery(this.elements.get(i));
-            var textarea = element.find('textarea');
-            element.show();
-            this.checkTextarea(textarea, false);
-            textarea.autosize({});
+
+        let filteredIndex = -1;
+        for(let j = 0; j < this.allRows.length; j++) {
+            if(this.filteredRows[j]) {
+                filteredIndex++;
+                if(filteredIndex < startIndex) continue;
+
+                let element = jQuery(this.allRows.get(j));
+                let textarea = element.find('textarea');
+                element.show();
+                this.checkTextarea(textarea, false);
+                autosize(textarea);
+
+                if(filteredIndex === endIndex-1) break;
+            }
         }
     };
 
     TranslationTable.prototype.setupEvents = function() {
-        var that = this;
-        this.elements.find('textarea').keyup(function() {
+        const that = this;
+        this.allRows.find('textarea').on('keyup', function() {
             that.checkTextarea($(this), true);
         });
 
-        this.elements.find('textarea').click(function() {
+        this.allRows.find('textarea').on('click', function() {
             that.checkTextarea($(this), true);
         });
 
-        this.elements.find('textarea').focusout(function() {
+        this.allRows.find('textarea').on('focusout', function() {
             $(this).popover('destroy');
         });
 
-        $(window).bind('beforeunload', function() {
+        $(window).on('beforeunload', function() {
             return "Do you really want to leave the page? All changes will be lost.";
         });
 
-        $('input[type=submit]').click(function() {
-            $(window).unbind('beforeunload');
+        $('button[type=submit]').on('click', function() {
+            $(window).off('beforeunload');
             if ($(this).attr('id') === 'save__button') {
                 that.filterText = '';
-                that.filter();
+                that.applyFilter();
             }
         });
     };
@@ -145,20 +157,20 @@ $(document).ready(function() {
 
         let form = $('<div class="col-md-4"></div>');
         form.append($('<label for="table-filter">Filter translations for:</label>'));
-        let filter = $('<input id="table-filter" type="text" />');
+        let filter = $('<input id="table-filter" type="text" class="form-control"/>');
         form.append(filter);
         parent.append(form);
 
-        var that = this;
+        const that = this;
         let changed = function() {
             let newVal = $(this).val();
-            if (that.filterText == newVal) {
+            if (that.filterText === newVal) {
                 return;
             }
             that.filterText = newVal;
             that.startOver();
         };
-        filter.keyup(changed);
+        filter.on('keyup', changed);
 
         parent.append($('<div class="col-md-6">This filter will search in both, translated and original text. '
             + 'Additionally, it will search in the path of the translated file and the translation keys.'
@@ -175,24 +187,24 @@ $(document).ready(function() {
 
     TranslationTable.prototype.drawPaginationBar = function() {
         this.pagination.empty();
-        let ul = $('<ul></ul>');
+        let ul = $('<ul class="pagination"></ul>');
         this.pagination.append(ul);
 
+
+        this.nextButton = this.paginationElement('»');
+        this.prevButton = this.paginationElement('«');
+
+        const that = this;
+        this.nextButton.on('click', function() {
+            that.nextPage();
+        });
+        this.prevButton.on('click', function() {
+            that.prevPage();
+        });
 
         if (this.page > 1) {
             ul.append(this.prevButton);
         }
-
-        this.nextButton = this.paginationElement('»', this.page+1);
-        this.prevButton = this.paginationElement('«', this.page-1);
-
-        var that = this;
-        this.nextButton.click(function() {
-            that.nextPage();
-        });
-        this.prevButton.click(function() {
-            that.prevPage();
-        });
 
         if (this.page <= this.paginationSpace + 3) {
             this.paginationPageElements(1, this.page + this.paginationSpace, ul);
@@ -204,14 +216,14 @@ $(document).ready(function() {
         if (this.page + 5 < this.lastPage) {
             ul.append(this.paginationSpacer());
         }
-        if (this.page + 5 == this.lastPage) {
+        if (this.page + 5 === this.lastPage) {
             ul.append(this.paginationPageElement(this.lastPage-1));
         }
         if (this.page + 4 <= this.lastPage) {
             ul.append(this.paginationPageElement(this.lastPage));
         }
 
-        if (this.page != this.lastPage) {
+        if (this.page !== this.lastPage) {
             ul.append(this.nextButton);
         }
     };
@@ -228,18 +240,25 @@ $(document).ready(function() {
     TranslationTable.prototype.paginationPageElement = function(page) {
         let element = this.paginationElement(page);
         let that = this;
-        element.click(function() {
+        element.on('click', function() {
             that.selectPage(jQuery(this).text());
         });
-        if (this.page == page) {
+        if (this.page === page) {
             element.addClass('active')
         }
         return element;
     };
 
     TranslationTable.prototype.paginationElement = function(text) {
+        let aria = '';
+        if(text === '«' ) {
+            aria = ' aria-label="Previous"';
+        }
+        if(text === '»' ) {
+            aria = ' aria-label="Next"';
+        }
         let page = $('<li></li>');
-        let link = $('<a></a>');
+        let link = $('<a' + aria + '></a>');
         link.text(text);
         page.append(link);
         return page;
@@ -257,7 +276,7 @@ $(document).ready(function() {
         if (newPage < 0) newPage = 0;
 
         this.page = newPage+1;
-        this.draw();
+        this.drawTable();
         this.drawPaginationBar();
         this.focusFirstTextarea();
     };
@@ -277,6 +296,11 @@ $(document).ready(function() {
         this.selectPage(this.page - 1);
     };
 
+    /**
+     * Checks textarea and warn if empty or when unequal number of placeholders and missing whitespace
+     * @param element
+     * @param popover
+     */
     TranslationTable.prototype.checkTextarea = function(element, popover) {
         let warnElement = element.parents('tr');
 
@@ -287,21 +311,24 @@ $(document).ready(function() {
         }
 
         let originalTranslation = warnElement.find('td:first');
+        //https://stackoverflow.com/a/8915445/1043588
         let regex = new RegExp(
-            "%[bcdeEufFgGosxX]{1}",
-            "g"
-        );
+                "%(?:\\d+\\$)?[+-]?(?:[ 0]|'.)?-?\\d*(?:\\.\\d+)?[bcdeEufFgGosxX]",
+                "g"
+            );
+
         let leftMatch = originalTranslation.text().match(regex);
         let rightMatch = element.val().match(regex);
         if (leftMatch != null || rightMatch != null) {
-
             if (rightMatch == null || leftMatch == null ||
                 rightMatch.length !== leftMatch.length) {
-                warnElement.addClass('warning');
+                warnElement.addClass('warning-msg');
                 element.popover({
                     placement: 'top',
                     trigger: 'manual',
-                    content: 'The translated text is missing a placeholder like %s or %d. It needs to be kept as is when translating.',
+                    content: 'The translated text is missing a placeholder like %s or %d. Placeholders needs to be kept when translating.<br><br>'
+                        + ' Tip: For changing of the order use %1$s and %2$s, where the numbers refer to their original position',
+                    html: true,
                     title: 'Missing placeholder'
                 });
                 if (popover) element.popover('show');
@@ -322,7 +349,7 @@ $(document).ready(function() {
         }
 
         if (leftSpaces !== rightSpaces) {
-            warnElement.addClass('warning');
+            warnElement.addClass('warning-msg');
             element.popover({
                 placement: 'top',
                 trigger: 'manual',
@@ -333,11 +360,10 @@ $(document).ready(function() {
             return;
         }
 
-        warnElement.removeClass('warning');
+        warnElement.removeClass('warning').removeClass('warning-msg');
         element.popover('destroy');
     };
 
 
-
-    var table = new TranslationTable();
+    const table = new TranslationTable();
 });
