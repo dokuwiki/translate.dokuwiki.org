695ffd6da77d90919691f164c4e76776f9e81abd
Andreas Gohr <andi@splitbrain.org>
2023-11-22 17:14:47 +0100

    fix foreignkey problem when updating collations
    On modern mariaDB instances collation changes can no longer ignore
    foreign keys. So they need to be dropped and readded.
    
    See https://jira.mariadb.org/browse/MDEV-31987

--- migrations/Version20230414172519.php
+++ migrations/Version20230414172519.php
@@ -17,37 +17,44 @@ final class Version20230414172519 extends AbstractMigration
         return 'Convert to a new char set';
     }
 
+
     public function up(Schema $schema): void
     {
-        //update database, needed?
+
+        $this->addSql('ALTER TABLE languageStats DROP FOREIGN KEY FK_F9AAD2F9D4DB71B5');
+        $this->addSql('ALTER TABLE languageStats DROP FOREIGN KEY FK_F9AAD2F950C9D4F7');
+        $this->addSql('ALTER TABLE translationUpdate DROP FOREIGN KEY FK_7241B4BD50C9D4F7');
+
         $this->addSql('ALTER DATABASE translate CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;');
-
-        //converting a table will changes all columns. If deviating char sets per columns, per column change would be needed.
-
         $this->addSql('ALTER TABLE doctrine_migration_versions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
 
-        $this->addSql('SET FOREIGN_KEY_CHECKS=0;');
         $this->addSql('ALTER TABLE languageStats CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
         $this->addSql('ALTER TABLE languageName CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
         $this->addSql('ALTER TABLE repository CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
         $this->addSql('ALTER TABLE translationUpdate CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
-        $this->addSql('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->addSql('ALTER TABLE languageStats ADD CONSTRAINT FK_F9AAD2F9D4DB71B5 FOREIGN KEY (language) REFERENCES languageName (code)');
+        $this->addSql('ALTER TABLE languageStats ADD CONSTRAINT FK_F9AAD2F950C9D4F7 FOREIGN KEY (repository_id) REFERENCES repository (id)');
+        $this->addSql('ALTER TABLE translationUpdate ADD CONSTRAINT FK_7241B4BD50C9D4F7 FOREIGN KEY (repository_id) REFERENCES repository (id)');
     }
 
     public function down(Schema $schema): void
     {
 
-        //update database, needed?
-        $this->addSql('ALTER DATABASE translate CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;');
+        $this->addSql('ALTER TABLE languageStats DROP FOREIGN KEY FK_F9AAD2F9D4DB71B5');
+        $this->addSql('ALTER TABLE languageStats DROP FOREIGN KEY FK_F9AAD2F950C9D4F7');
+        $this->addSql('ALTER TABLE translationUpdate DROP FOREIGN KEY FK_7241B4BD50C9D4F7');
 
-        //convert table, changes all columns. If deviating columns, per column change would be needed.
+        $this->addSql('ALTER DATABASE translate CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;');
         $this->addSql('ALTER TABLE doctrine_migration_versions CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;');
 
-        $this->addSql('SET FOREIGN_KEY_CHECKS=0;');
         $this->addSql('ALTER TABLE languageStats CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;');
         $this->addSql('ALTER TABLE languageName CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;');
         $this->addSql('ALTER TABLE repository CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;');
         $this->addSql('ALTER TABLE translationUpdate CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;');
-        $this->addSql('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->addSql('ALTER TABLE languageStats ADD CONSTRAINT FK_F9AAD2F9D4DB71B5 FOREIGN KEY (language) REFERENCES languageName (code)');
+        $this->addSql('ALTER TABLE languageStats ADD CONSTRAINT FK_F9AAD2F950C9D4F7 FOREIGN KEY (repository_id) REFERENCES repository (id)');
+        $this->addSql('ALTER TABLE translationUpdate ADD CONSTRAINT FK_7241B4BD50C9D4F7 FOREIGN KEY (repository_id) REFERENCES repository (id)');
     }
 }
